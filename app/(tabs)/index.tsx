b2d761165e119040d5051360ef640675bd59d671
Muaz Hazali <100086292+muazhazali@users.noreply.github.com>
2025-01-27 00:02:54 +0800

    🍻 update to do list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,74 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useState } from "react";
+import { VStack } from "@/components/ui/vstack";
+import { FormControl } from "@/components/ui/form-control";
+import { Input, InputField, InputIcon } from "@/components/ui/input";
+import { AddIcon } from "@/components/ui/icon";
+import { Pressable } from "@/components/ui/pressable";
+import { defaultTodos } from "../../constants/todo";
+import TodoContainer, { Todo } from "@/components/app-components/TodoContainer";
+import shortid from "shortid";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const Home = () => {
+  const [item, setItem] = useState("");
+  const [todos, setTodos] = useState<Todo[]>(defaultTodos);
 
-export default function HomeScreen() {
+  const addTodo = (task: string) => {
+    const lastTodo = todos[todos?.length - 1];
+    if (lastTodo?.task !== "" && task !== "") {
+      setTodos([
+        ...todos,
+        {
+          id: shortid.generate(),
+          task: task,
+          completed: false,
+        },
+      ]);
+      setItem("");
+    }
+  };
+
+  const toggleTodo = (id: string) => {
+    const updatedTodos = todos?.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id: string) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <VStack className="flex-1 bg-secondary-100 md:bg-secondary-0 md:items-center md:justify-center ">
+      <VStack className="rounded-md bg-secondary-100 md:h-[500px] md:w-[700px]">
+        <FormControl className="my-4">
+          <Input variant="underlined" size="sm" className="mx-6 my-2">
+            <InputField
+              placeholder="What is your next task?"
+              value={item}
+              onChangeText={(value) => setItem(value)}
+              onSubmitEditing={() => addTodo(item)}
+            />
+            <Pressable onPress={() => addTodo(item)}>
+              <InputIcon as={AddIcon} className="cursor-pointer h-3 w-3" />
+            </Pressable>
+          </Input>
+        </FormControl>
+        {todos?.map((todo: Todo, index: number) => (
+          <TodoContainer
+            key={index}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </VStack>
+    </VStack>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default Home;
